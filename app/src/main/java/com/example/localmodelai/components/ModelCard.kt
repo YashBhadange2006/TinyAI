d68ffbad58509224c76750bd58ecd7fb63173368
YashBhadange2006 <yash271106@gmail.com>
2026-06-04 23:23:10 +0530

    Added Initial System Prompt UI in card inside  advance setting

--- app/src/main/java/com/example/localmodelai/components/ModelCard.kt
+++ app/src/main/java/com/example/localmodelai/components/ModelCard.kt
@@ -9,7 +9,13 @@ import android.os.Environment
 import android.os.StatFs
 import android.text.format.Formatter
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,8 +27,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.AutoAwesome
 import androidx.compose.material.icons.filled.DeleteOutline
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.PhoneAndroid
 import androidx.compose.material.icons.filled.Storage
 import androidx.compose.material3.Button
@@ -33,16 +41,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,7 +75,7 @@ data class StorageInfo(
 @Composable
 fun StorageCard() {
     val context = LocalContext.current
-    val deviceName = "${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL}"
+    val deviceName = "${Build.MANUFACTURER} ${Build.MODEL}"
 
     fun getDetailedStorage(ctx: Context): com.example.localmodelai.components.StorageInfo {
         return try {
@@ -184,6 +199,14 @@ fun ModelItemRow(
     isLoading: Boolean,
     isLoaded: Boolean
 ) {
+
+    var isExpanded by remember { mutableStateOf(false)}
+
+    val arrowRotationState by animateFloatAsState(
+        targetValue = if(isExpanded) 180f else 0f,
+        label ="ArrowRotation"
+    )
+
     Card(
         shape = RoundedCornerShape(28.dp),
         colors = CardDefaults.cardColors(
@@ -192,7 +215,12 @@ fun ModelItemRow(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp)
+            .padding(5.dp),
+        onClick = {
+            if(isLoading){
+                isExpanded = !isExpanded
+            }
+        }
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
@@ -369,6 +397,81 @@ fun ModelItemRow(
                             )
                         }
                     }
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .animateContentSize(
+                                animationSpec = tween(
+                                    durationMillis = 300,
+                                    easing = LinearOutSlowInEasing
+                                )
+                            )
+                    ){
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable(
+                                    interactionSource = remember { MutableInteractionSource()},
+                                    indication = null
+                                ){
+                                    isExpanded = !isExpanded
+                                },
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center,
+                        ){
+                            Text(
+                                text = "Advance Settings",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+
+                            Icon(
+                                imageVector = Icons.Default.KeyboardArrowDown,
+                                contentDescription = "Expand/Collapse",
+                                modifier = Modifier.rotate(arrowRotationState)
+                            )
+                        }
+                        var systemPrompt by remember { mutableStateOf("") }
+
+                        if(isExpanded){
+                            Text(
+                                text = "System Prompt:",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(top = 8.dp, bottom = 6.dp)
+                            )
+
+                            OutlinedTextField(
+                                value = systemPrompt,
+                                onValueChange = { systemPrompt = it },
+
+                                modifier = Modifier.fillMaxWidth(),
+                                enabled = true,
+                                placeholder = {Text(
+                                    "Enter your system prompt",
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                                ) },
+                                shape = RoundedCornerShape(25.dp),
+                                singleLine = false,
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    // Transparent border
+                                    focusedBorderColor = Color.Transparent,
+                                    unfocusedBorderColor = Color.Transparent,
+                                    disabledBorderColor = Color.Transparent,
+                                    // Grey container background inside search bar
+                                    focusedContainerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.6f),
+                                    unfocusedContainerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.4f),
+                                    // icon matching theme color
+                                    focusedLeadingIconColor = MaterialTheme.colorScheme.onSurface,
+                                    unfocusedLeadingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                    focusedTrailingIconColor = MaterialTheme.colorScheme.onSurface,
+                                    unfocusedTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+
+                        }
+                    }
                 }
                 else -> {
                     Button(
@@ -406,7 +509,7 @@ fun ModelItemRowPreview() {
 
     val mockStatus = ModelDownloadStatus(
         isDownloading = false,
-        isDownloaded = false,
+        isDownloaded = true,
         progressPercent = 100,
         statusMessage = "Ready"
     )
