af317ad6e9881c735a79912ffee61d4530023a5e
YashBhadange2006 <yash271106@gmail.com>
2026-06-27 14:48:18 +0530

    Comments clean-up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,14 +22,6 @@ android {
     }
 
     buildTypes {
-//        debug {
-//            isMinifyEnabled = true
-//            isShrinkResources = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
